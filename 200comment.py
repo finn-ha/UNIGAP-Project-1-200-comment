8f0d09ed7d81be801feda1a4e98a236429f0d74c
PC-WATEC <vinhlong276@gmail.com>
2024-11-05 16:53:02 +0700

    version 1

--- 200comment.py
+++ 200comment.py
@@ -3,8 +3,9 @@ from venv import create
 from sqlalchemy import create_engine
 
 # Đọc file Excel
-xlsx_path = 'H:/My Drive/Learn Python/comment.xlsx'  # Dùng / để tránh lỗi escape character
+xlsx_path = 'H:/My Drive/Learn Python/comment 7 col.xlsx'  # Dùng / để tránh lỗi escape character
 df = pd.read_excel(xlsx_path)
+df = df.drop_duplicates()
 
 # Lọc dữ liệu theo từ khóa
 key = "vinh|tiên|vc|vợ chồng|vk ck|từ thiện|cứu trợ|đồng bào|tỷ|vc|quyên góp|miền trung|đồng bào|cv|tỷ|kiện|Hằng|lux"
@@ -22,5 +23,5 @@ url = f"mysql+pymysql://{username}:{password}@localhost/{db}"
 engine = create_engine(url)
 
 # Đẩy dữ liệu lên MySQL
-df.to_sql('filtered_data', con=engine, if_exists='replace', index=False)
+df.to_sql('comment', con=engine, if_exists='replace', index=False)
 print(df)
